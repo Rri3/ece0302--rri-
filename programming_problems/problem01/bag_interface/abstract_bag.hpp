b4b9ba771f41012238b79fbbfea9eab7a05bead6
Rafik <rri3@pitt.edu>
2023-01-21 17:18:01 -0500

    prob 1

--- programming_problems/problem01/bag_interface/abstract_bag.hpp
+++ programming_problems/problem01/bag_interface/abstract_bag.hpp
@@ -3,7 +3,16 @@
 
 #include <cstdlib>
 
-//TODO
+template<typename T>class AbstractBag
+{
+virtual size_t getCurrentSize() const = 0;
+virtual bool isEmpty() const = 0;
+virtual bool add(const T& entry) = 0;
+virtual bool remove(const T& entry) = 0;
+virtual void clear() = 0;
+virtual size_t getFrequencyOf(const T& entry) const = 0;
+virtual bool contains(const T& entry) const = 0;
+};
 
 
 #endif
